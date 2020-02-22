25e6604381515e848a2192cc5a95f8480c268e46
Nina Di Cara <ninadicara@gmail.com>
2020-02-22 14:32:12 +0000

    Fix TypeError for daily scheduling

--- bot-main.py
+++ bot-main.py
@@ -36,16 +36,33 @@ def start(update, context):
     bot.send_message(chat_id=update.message.chat_id,
                      text="Welcome to the University of Bristol's Mood Music Study! " "Thanks so much for taking part. " "Please remember to enable push notifications from Telegram, and when that is done reply /ready.")
 
+def daily_message(context: telegram.ext.CallbackContext):
+     context.bot.send_message(chat_id=context.job.context, text='This is a sheduled message')
+
+def callback_daily(update: telegram.Update, context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text="Setting up your daily surveys!")
+    t = datetime.time(14, 29, 00, 000000)
+    context.job_queue.run_daily(daily_message, t, context=update.message.chat_id)
+
 #Function that will run the EMA survey at the times we want every day. 
-def daily_job(update, job_queue):
-    """ Running on Mon, Tue, Wed, Thu, Fri, Sat, Sun = tuple(range(7)) """
-    bot.send_message(chat_id=update.message.chat_id, text="Great, you\'re all set up! " "You will receive a message when it\'s time for your next survey. If you don\'t want to wait you can try in out now by replying /ema. " "Have a good day :)")
-    t = datetime.time(20, 16, 00, 000000)
-    job_queue.run_daily(ema_survey, t, days=tuple(range(7)), context=update)
+# def daily_job(update, job_queue):
+#     """ Running on Mon, Tue, Wed, Thu, Fri, Sat, Sun = tuple(range(7)) """
+#     bot.send_message(chat_id=update.message.chat_id, text="Great, you\'re all set up! " "You will receive a message when it\'s time for your next survey. If you don\'t want to wait you can try it out now by replying /ema. " "Have a good day :)")
+#     t = datetime.time(20, 16, 00, 000000)
+#     job_queue.run_daily(ema_survey, t, days=tuple(range(7)), context=job.context)
 
 #Function to manage unrecognised commands
-def unknown(update, context):
+def unknowncommand(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command")
+
+#Function to manage unrecognised commands
+def unknowntext(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I\'m not able to understand what you have said. Please see /help for an overview of my commands, or wait until your next survey. Thank you!")
+
+#Function to manage unrecognised commands
+def helpme(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Soon this will show you all my available commands!")
 
 
 #Initialise the states for conversation handler - NameError saying they aren't defined otherwise 
@@ -98,10 +115,7 @@ def main():
 
     dispatcher = updater.dispatcher
 
-    #Initialise dataframe to store survey responses in.
-    #df = pd.DataFrame(columns=['chat_id', 'timestamp', 'reply'])
-
-    #j = updater.job_queue
+    job_queue = updater.job_queue
 
     ##########################
     ## CONVERSATION HANDLER ##
@@ -111,9 +125,9 @@ def main():
         entry_points=[CommandHandler('ema', ema_start)],
 
         states={
-            HAPPINESS: [MessageHandler(Filters.regex('[0-9]|10'), ema_happiness)],
+            HAPPINESS: [MessageHandler(Filters.regex('[1-9]|10'), ema_happiness)],
 
-            ENERGY: [MessageHandler(Filters.regex('[0-9]|10'), ema_energy)]
+            ENERGY: [MessageHandler(Filters.regex('[1-9]|10'), ema_energy)]
         },
 
         fallbacks = []
@@ -127,16 +141,19 @@ def main():
     dispatcher.add_handler(ema_handler)
 
     #Ready Handler which sets up the JoQueue
-    ready_handler = CommandHandler('ready', daily_job, pass_job_queue=True)
-    dispatcher.add_handler(ready_handler)
+    dispatcher.add_handler(CommandHandler('ready', callback_daily, pass_job_queue=True))
 
     #Start Handler - responds to the \start message using the start function. 
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(CommandHandler('start', start))
 
-    #Unknown Handler - 
-    unknown_handler = MessageHandler(Filters.command, unknown)
-    dispatcher.add_handler(unknown_handler)
+    ##Help Command Handler - 
+    dispatcher.add_handler(CommandHandler('help', helpme))
+
+    #Unknown Command Handler - 
+    dispatcher.add_handler(MessageHandler(Filters.command, unknowncommand))
+
+    #Unknown Anything Handler - 
+    dispatcher.add_handler(MessageHandler(Filters.text, unknowntext))
 
     #Set Bot running
     updater.start_polling()
